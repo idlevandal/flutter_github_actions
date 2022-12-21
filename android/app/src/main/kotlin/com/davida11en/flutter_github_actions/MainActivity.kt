38b723897c6c7561e9360f2f611156724bc9d69e
David Allen <davetvmusic@hotmail.com>
2022-12-21 13:00:32 +1000

    initial commit

---
+++ android/app/src/main/kotlin/com/davida11en/flutter_github_actions/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.davida11en.flutter_github_actions
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AppBar'),
+      ),
+      body: const Center(
+        child: Text('Home Page', style: TextStyle(fontSize: 40.0, color: Colors.blue),),
+      ),
+    );
+  }
+}
+
+
+
